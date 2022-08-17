c51b4ff225853a069174dafd8b4f894849038e8c
Jeanne <jeanneclarissetoledo@gmail.com>
2022-08-17 22:03:37 +0800

    Enable inputs to be displayed on the calculator's screen

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 function checkExpression(e) {
+    defaultConfiguration = false;
     if (operations.length == 0) {
         a += e.target.getAttribute("id");
+        calcContents += e.target.getAttribute("id");
+        updateScreenDisplay();
     }
     else {
         b += e.target.getAttribute("id");
+        calcContents += e.target.getAttribute("id");
+        updateScreenDisplay();
     }
 }
 
 function toggleOperator(e) {
     operations.push(e.target.getAttribute("id"));
+    calcContents += ` ${e.target.getAttribute("id")} `;
+    updateScreenDisplay();
 
     if (operations.length > 1) {
         operate();
     } 
+
+    if (!defaultConfiguration) {
+        calcContents = `${a} ${e.target.getAttribute("id")} `;
+        updateScreenDisplay();
+        currentInput.textContent = "";
+    }
+
+
+}
+
+function updateScreenDisplay() {
+    previousInput.textContent = calcContents;
 }
 
 function operate() {
     switch(operations[0]) {
-        case ("add"): 
+        case ("+"): 
             result = add(Number(a), Number(b));
             break;
-        case("subtract"): 
+        case("-"): 
             result = subtract(Number(a), Number(b));
             break;
-        case("multiply"):
+        case("*"):
             result = multiply(Number(a), Number(b));
             break;
-        case("divide"):
+        case("/"):
             result = divide(Number(a), Number(b));
             break;
     }
     console.log(`${a} ${operations[0]} ${b} = ${result}`);
+    currentInput.textContent = result;
     configure();
 }
 
@@ -54,12 +74,23 @@ function configure() {
     a = result;
     b = "";
     operations.shift();
+
+    if (operations.length > 1) {
+        calcContents = `${a} ${operations[0]}`;
+        previousInput.textContent = calcContents;
+        currentInput.textContent = "";
+    }
+    
+    
 }
 
 let a = "";
 let b = "";
 let operations = [];
 let result;
+let defaultConfiguration = true;
+
+let calcContents = "";
 
 const numbers = document.querySelectorAll(".number");
 numbers.forEach(number => number.addEventListener("click", checkExpression));
@@ -69,10 +100,21 @@ operators.forEach(op => op.addEventListener("click", toggleOperator));
 
 const evaluate = document.querySelector(".eval");
 evaluate.addEventListener("click", operate);
+evaluate.addEventListener("click", (e) => {
+    calcContents += ` ${e.target.getAttribute("id")}`;
+    updateScreenDisplay();
+});
 
 const ac = document.querySelector("#clear");
 ac.addEventListener("click", () => {
     a = "";
     b = "";
     operations = [];
-})
+    calcContents = "";
+    previousInput.textContent = "";
+    currentInput.textContent = "";
+    defaultConfiguration = true;
+});
+
+const previousInput = document.querySelector("#prev-input");
+const currentInput = document.querySelector("#current-input");
